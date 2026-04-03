b6053389c5f138b5eecf9bdcc293b10dc191b610
Yayzxer <yanniortapias@hotmail.com>
2026-04-02 21:48:26 -0500

    Arreglando bug

---
+++ BackEnd/src/test/java/com/gpsromp/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gpsromp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
