612c41fc2ff1cf5c5f4b32363fa4b08e4d588fd4
YayzxerProgramer <yanniortapiass@tecnocomfenalco.edu.co>
2026-05-13 08:54:03 -0500

    Cambios en WebSocket

--- BackEnd/src/main/java/com/gpsromp/WebSocket/WebSocketConfig.java
+++ BackEnd/src/main/java/com/gpsromp/WebSocket/WebSocketConfig.java
@@ -1,4 +1,4 @@
-package com.gpsromp.WebSocket;
+package com.gpsromp.websocket;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
