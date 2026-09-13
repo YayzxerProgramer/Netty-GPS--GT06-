556d406ef48e7bba267e70759dea071bb8c31c99
Chxng07 <Jchangmoreno5@gmail.com>
2026-09-13 12:02:45 -0500

    brrr

--- BackEnd/src/main/java/com/gpsromp/WebSocket/WebSocketConfig.java
+++ BackEnd/src/main/java/com/gpsromp/WebSocket/WebSocketConfig.java
@@ -1,4 +1,4 @@
-package com.gpsromp.websocket;
+package com.gpsromp.WebSocket;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
